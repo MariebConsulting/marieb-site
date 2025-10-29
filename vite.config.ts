644414228b65b159679ad45e07b9536846ebcfea
MariebConsulting <Daniel@epples.net>
2025-10-29 17:00:33 -0400

    Change base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'marieb.consulting', // custom domain -> '/'
+  base: '/', // custom domain -> '/'
 })
